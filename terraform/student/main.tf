1d90cc1382bac08d03834036511ee0bff14aa529
Aidan Feldman <aidan.feldman@gmail.com>
2023-12-26 02:21:33 -0500

    prevent destroying student Projects

--- terraform/student/main.tf
+++ terraform/student/main.tf
@@ -2,4 +2,8 @@ resource "google_project" "student" {
   name       = "Ops Mgmt in IT - ${var.uni}"
   project_id = "columbia-ops-mgmt-${var.uni}"
   org_id     = var.org_id
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
