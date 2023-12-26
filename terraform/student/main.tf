6b0b3bd48de25f6d96d26c702d94d1767770895c
Aidan Feldman <aidan.feldman@gmail.com>
2023-12-26 00:53:02 -0500

    move student IAM stuff to its own file

--- terraform/student/main.tf
+++ terraform/student/main.tf
@@ -3,25 +3,3 @@ resource "google_project" "student" {
   project_id = "columbia-ops-mgmt-${var.uni}"
   org_id     = var.org_id
 }
-
-locals {
-  email = coalesce(var.email_override, "${var.uni}@columbia.edu")
-}
-
-resource "google_project_iam_member" "student" {
-  project = google_project.student.project_id
-  role    = "roles/editor"
-  member  = "user:${local.email}"
-}
-
-resource "google_project_iam_member" "associates" {
-  for_each = toset([
-    "as6950",
-    "cv2464",
-    "vv2358"
-  ])
-
-  project = google_project.student.project_id
-  role    = "roles/editor"
-  member  = "user:${each.key}@columbia.edu"
-}
