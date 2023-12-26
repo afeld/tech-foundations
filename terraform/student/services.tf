bd67b8216ef270512c68a02d0c802bd876a3a874
Aidan Feldman <aidan.feldman@gmail.com>
2023-12-26 03:05:08 -0500

    don't allow students to remove the instructors

--- terraform/student/services.tf
+++ terraform/student/services.tf
@@ -3,7 +3,9 @@ resource "google_project_service" "default" {
 
   for_each = toset([
     "analyticshub.googleapis.com",
-    "bigquery.googleapis.com"
+    "bigquery.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com"
   ])
   service = each.value
 }
