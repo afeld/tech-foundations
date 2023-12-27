3fe8fd08aea74128b7f0e632b11012310bcbbb49
Aidan Feldman <aidan.feldman@gmail.com>
2023-12-27 15:56:34 -0500

    use Google Group for Associates

--- terraform/student/iam.tf
+++ terraform/student/iam.tf
@@ -11,13 +11,7 @@ resource "google_project_iam_member" "student" {
 # creator (person running Terraform) automatically added as an Owner
 
 resource "google_project_iam_member" "associates" {
-  for_each = toset([
-    "as6950",
-    "cv2464",
-    "vv2358"
-  ])
-
   project = google_project.student.project_id
   role    = "roles/owner"
-  member  = "user:${each.key}@columbia.edu"
+  member  = "group:opsmgmtinitinstructors@columbia.edu"
 }
