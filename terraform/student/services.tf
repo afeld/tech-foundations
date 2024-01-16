9910e6f0d14c5bbd644be69e0fde4a8055aad8c3
Aidan Feldman <aidan.feldman@gmail.com>
2024-01-16 18:53:06 -0500

    only enable services if there's a billing account

--- terraform/student/services.tf
+++ terraform/student/services.tf
@@ -1,7 +1,9 @@
 resource "google_project_service" "default" {
   project = google_project.student.project_id
 
-  for_each = toset([
+  # some services require a billing account to be associated, so don't bother enabling them if we don't have one
+  # https://support.google.com/googleapi/answer/6158867
+  for_each = toset(google_project.student.billing_account == null ? [] : [
     "analyticshub.googleapis.com",
     "bigquery.googleapis.com",
     "cloudaicompanion.googleapis.com",
