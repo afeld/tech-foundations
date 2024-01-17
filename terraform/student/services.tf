bd93d90f12a6571737beed00e088e9f7a3e0302a
Aidan Feldman <aidan.feldman@gmail.com>
2024-01-16 19:03:06 -0500

    only enable certain services if billing account is associated

--- terraform/student/services.tf
+++ terraform/student/services.tf
@@ -1,14 +1,23 @@
-resource "google_project_service" "default" {
-  project = google_project.student.project_id
+locals {
+  has_billing_account = google_project.student.billing_account != null
 
-  # some services require a billing account to be associated, so don't bother enabling them if we don't have one
+  # some services require a billing account to be associated
   # https://support.google.com/googleapi/answer/6158867
-  for_each = toset(google_project.student.billing_account == null ? [] : [
+  services_that_require_billing = [
+    "compute.googleapis.com",
+  ]
+  services_to_enable = setunion([
     "analyticshub.googleapis.com",
     "bigquery.googleapis.com",
     "cloudaicompanion.googleapis.com",
-    "compute.googleapis.com",
     "sqladmin.googleapis.com",
-  ])
-  service = each.value
+  ], local.has_billing_account ? local.services_that_require_billing : [])
+}
+
+resource "google_project_service" "default" {
+  project = google_project.student.project_id
+
+  for_each                   = local.services_to_enable
+  service                    = each.value
+  disable_dependent_services = true
 }
