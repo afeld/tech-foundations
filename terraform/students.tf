6f4add222adf860d643403971eae55659145dc01
Aidan Feldman <aidan.feldman@gmail.com>
2024-01-17 15:53:34 -0500

    report on the users without billing accounts

--- terraform/students.tf
+++ terraform/students.tf
@@ -21,8 +21,24 @@ module "test_student" {
   email_override = "aidan.feldman@gmail.com"
 }
 
+# reporting on billing accounts
+
 output "billing_accounts" {
   value = {
     for uni, student in module.students : uni => student.billing_account
   }
+}
+
+locals {
+  num_projects = length(module.students)
+  unis_without_billing_account = compact([for uni, student in module.students : student.billing_account == null ? uni : null
+  ])
+}
+
+output "unis_without_billing_account" {
+  value = local.unis_without_billing_account
+}
+
+output "fraction_with_billing_accounts" {
+  value = 1 - length(local.unis_without_billing_account) / local.num_projects
 }
