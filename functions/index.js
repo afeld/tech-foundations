d11ed25a11894adf478f964e9a9dd1541b288a3b
Aidan Feldman <aidan.feldman@gmail.com>
2024-03-06 01:53:08 -0500

    write user data

--- functions/index.js
+++ functions/index.js
@@ -8,22 +8,32 @@ const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp();
 
+const db = getFirestore();
+
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
 exports.addmessage = onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await getFirestore()
-    .collection("messages")
-    .add({ original: original });
+  const uni = "alf2215";
+  const data = {
+    app_engine: null,
+    app_engine_200: new Date(),
+    repo: null,
+    build_trigger: null,
+    build: null,
+    build_success: null,
+  };
+
+  const writeResult = await db
+    .collection("continuous_deployment_checks")
+    .doc(uni)
+    .set(data);
   // Send back a message that we've successfully written the message
   res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
 
 // https://cloud.google.com/appengine/docs/flexible/scheduling-jobs-with-cron-yaml#formatting_the_schedule
-exports.scheduled = onSchedule("every 1 minutes", async (event) => {
-  await getFirestore().collection("messages").add({ text: "written" });
+// exports.scheduled = onSchedule("every 1 minutes", async (event) => {
+//   await getFirestore().collection("messages").add({ text: "written" });
 
-  logger.log("User cleanup finished");
-});
+//   logger.log("User cleanup finished");
+// });
