38d23d6fe8d272c0f4a0f82dc4a5b03bf58d4161
Aidan Feldman <aidan.feldman@gmail.com>
2024-03-23 15:16:03 -0400

    compute completion dates

--- cd_assignment/assemble.js
+++ cd_assignment/assemble.js
@@ -1,6 +1,10 @@
+import fs from "node:fs";
 import { readdir } from "node:fs/promises";
+import { parse } from "csv-parse";
+import { DateTime } from "luxon";
 
 const files = await readdir("./");
+files.sort();
 
 const getResultDate = (file) => {
   const matches = file.match(/^cd_results_(\d{4})-(\d{2})-(\d{2})\.csv$/);
@@ -8,14 +12,47 @@ const getResultDate = (file) => {
     return null;
   }
 
-  return new Date(matches[1], matches[2] - 1, matches[3]);
+  return DateTime.fromObject({
+    year: matches[1],
+    month: matches[2],
+    day: matches[3],
+  });
 };
+
+const students = {};
 
 for (const file of files) {
   console.log("Processing file:", file);
+
   const resultDate = getResultDate(file);
   if (!resultDate) {
+    // not a result file
     continue;
   }
-  console.log(resultDate);
+
+  const parser = fs.createReadStream(file).pipe(parse({ columns: true }));
+  for await (const row of parser) {
+    // initialize
+    students[row.uni] = students[row.uni] || {};
+
+    const checks = Object.keys(row).filter((key) => key !== "uni");
+    console.log(checks);
+
+    for (const check of checks) {
+      const existingDate = students[row.uni][check];
+      if (!existingDate) {
+        // not previously completed
+
+        if (row[check] === "Y") {
+          // completed on this day
+          students[row.uni][check] = resultDate.toISODate();
+        } else {
+          // still not completed
+          students[row.uni][check] = null;
+        }
+      }
+    }
+  }
 }
+
+console.log(students);
