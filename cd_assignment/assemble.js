da2da0f22663a03701adf4616aba6b15fe5d0783
Aidan Feldman <aidan.feldman@gmail.com>
2024-03-23 14:36:05 -0400

    extract the result date

--- cd_assignment/assemble.js
+++ cd_assignment/assemble.js
@@ -1,9 +1,21 @@
 import { readdir } from "node:fs/promises";
-import path from "path";
 
 const files = await readdir("./");
-const csvFiles = files.filter((file) => path.extname(file) === ".csv");
 
-for (const file of csvFiles) {
+const getResultDate = (file) => {
+  const matches = file.match(/^cd_results_(\d{4})-(\d{2})-(\d{2})\.csv$/);
+  if (!matches) {
+    return null;
+  }
+
+  return new Date(matches[1], matches[2] - 1, matches[3]);
+};
+
+for (const file of files) {
   console.log("Processing file:", file);
+  const resultDate = getResultDate(file);
+  if (!resultDate) {
+    continue;
+  }
+  console.log(resultDate);
 }
